a32e34e76d89143f5bbfcaa64ca6092ccef815b8
Braden0006 <bradenwhitch@yahoo.com>
2022-05-20 09:27:34 -0600

    Added state to the increment and decrement button for the quantity

--- src/components/ViewCart/ViewCart.js
+++ src/components/ViewCart/ViewCart.js
@@ -1,16 +1,40 @@
-import React from "react";
-import './ViewCart.css'
+import React, { useState } from "react";
+import "./ViewCart.css";
 
 export default function () {
+  const [quantity, setQuantity] = useState(0);
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity === 0) {
+      setQuantity(quantity);
+    } else {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
     <>
       <div className="viewcart-container">
         <div className="item-container">
           <span className="viewcart_item">Item</span>
           <div className="button_container">
-            <button className="viewcart_increment">+</button>
-            <span className="viewcart_quantity">1</span>
-            <button className="viewcart_decrement">-</button>
+            <button
+              className="viewcart_increment"
+              onClick={incrementQuantity}
+            >
+              +
+            </button>
+            <span className="viewcart_quantity">{quantity}</span>
+            <button
+              className="viewcart_decrement"
+              onClick={decrementQuantity}
+            >
+              -
+            </button>
           </div>
         </div>
       </div>
