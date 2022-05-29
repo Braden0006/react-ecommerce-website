022370ebdc54f899e2c346655bddf7ddab9dc4c8
Braden0006 <bradenwhitch@yahoo.com>
2022-05-28 19:01:56 -0600

    Made media query for all components

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,11 +6,13 @@ export default function HomePage() {
   return (
     <>
       <div className="homepage-container">
-        <div className="homepage-title-container">
-          <span className="homepage_title">Succulents from around the globe</span>
-          <span className="homepage_title">shipped right to your door</span>
+        <div className="homepage_title_button">
+          <div className="homepage-title-container">
+            <span className="homepage_title">Succulents from around the globe</span>
+            <span className="homepage_title">shipped right to your door</span>
+          </div>
+          <Link to='#shop' smooth><button className="homepage_button">Shop Now</button></Link>
         </div>
-        <Link to='#shop' smooth><button className="homepage_button">Shop Now</button></Link>
       </div>
     </>
   );
--- src/components/ViewCart/ViewCart.js
+++ src/components/ViewCart/ViewCart.js
@@ -41,33 +41,35 @@ export default function ViewCart() {
         <h1>Items ({totalItems})</h1>
 
         {/* Maps over the items object, which was created by the "useCart" API by adding the plant object with the "useCart" when I looped over them to create the cards in the "ProductPageCard" page */}
-        {items.map((item) => (
-          <div className="item-container" key={item.id}>
-            <h4 className="viewcart_item">{item.name}</h4>
-            <span
-              className={`img_container ${
-                item.id === 1 ? "jade_plant" : "aloe_vera"
-              }`}
-            ></span>
-            <div className="button_container">
-              <button
-                className="viewcart_increment"
-                onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
-              >
-                +
-              </button>
-              <span className="viewcart_quantity">{item.quantity}</span>
-              <button
-                className="viewcart_decrement"
-                onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
-              >
-                -
-              </button>
+        <div className="item">
+          {items.map((item) => (
+            <div className="item-container" key={item.id}>
+              <h4 className="viewcart_item">{item.name}</h4>
+              <span
+                className={`img_container ${
+                  item.id === 1 ? "jade_plant" : "aloe_vera"
+                }`}
+              ></span>
+              <div className="button_container">
+                <button
+                  className="viewcart_increment"
+                  onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+                >
+                  +
+                </button>
+                <span className="viewcart_quantity">{item.quantity}</span>
+                <button
+                  className="viewcart_decrement"
+                  onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+                >
+                  -
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
         <span className="cart-total">Total: ${cartTotal.toFixed(2)}</span>
-        <button onClick={() => emptyCart()}>Remove All Items</button>
+        <button className="remove-item_btn" onClick={() => emptyCart()}>Remove All Items</button>
       </div>
     </>
   );
