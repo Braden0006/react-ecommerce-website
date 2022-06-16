71c3c20d96c1c80c592993c02d408df4c446be01
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-15 19:19:21 -0600

    Added basename to router tag

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* Wrapped the "App" component with the Router tags so we can navigate to different parts of the components with links */}
-    <Router>
+    <Router basename="/react-ecommerce-website">
       <App />
     </Router>
   </React.StrictMode>
